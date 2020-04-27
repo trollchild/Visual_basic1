93c0cee84482f9c6e8c4a9747a95cb080e0cd173
Niclas Pettersson <40391901+trollchild@users.noreply.github.com>
2020-04-27 22:18:30 +0300

    Question app

--- ConsoleApp1/Module1.vb
+++ ConsoleApp1/Module1.vb
@@ -2,6 +2,23 @@
 
     Sub Main()
 
+        Dim name As String
+        Dim answer As String
+
+        Console.WriteLine("Hello what is your name?")
+        name = Console.ReadLine()
+
+        Console.WriteLine("Hello " & name)
+
+        Console.WriteLine("How was your day?")
+        answer = Console.ReadLine()
+
+        Console.WriteLine("So you say your day was " & answer)
+        Console.Wait(Now + TimeValue("0:00:10"))
+        Console.WriteLine("So if I understood correctly...")
+
+        Console.ReadKey(True)
+
     End Sub
 
 End Module
